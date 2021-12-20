3c02c04e803c395ef2b03dfd23ac0860a7a815ea
Natalia Guerrero <nathaliiaa_best@hotmail.com>
2021-12-20 16:35:18 +0100

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'creador';
+  fondoimagen: string = "https://www.tcgplayer.it/public/mario/images/CFC-016-U-Itaqweqqsda.jpg";
+  casa: string = "Fenrir";
+  casa2: string = "Ninguna";
+  ninguna: string = "Ninguna"
+  mercenario: string = "Mercenario"
+  nombrecarta: string = "";
+  casas: Array<string> = ["Borcloy", "Fenrir", "Rutghar", "Yalmir"];
+  imagencreada = "";
+  tiposdecarta : Array<string> = ["Contendiente", "Táctica"];
+  tipodecarta : string = "" ;
+  subtiposdecarta : Array <string> = ["Pistolero","Espadachín","Hechicero","Guerrero","Mago","Cazador","Bestia","Caballero","Ladrón","Místico"];
+  subtipodecarta : string = "";
+  filePath: string = "";
+  myForm: FormGroup;
+  textodeCarta: string = "" ; 
+  constructor(public fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      img: [null],
+      filename: ['']
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  crearEtiquetas() : void {
+    let fly = document.createElement("span");
+    let flytext = document.createTextNode("Volar");
+    let carta = document.getElementById("textoCarta")
+    fly.appendChild(flytext);
+   
+  }
+  textoCarta(e : string) {
+    let completedText = e
+    document.querySelector('#textoCarta')!.innerHTML = completedText
+  }
+  comprobarCasa(e: string) : void {
+    this.casa = e
+    if (e === this.casa2) {
+      this.casa2 = this.ninguna
+    } if (e === this.mercenario) {
+      this.casa2 = this.ninguna
+    }
+  }
+
+  imagePreview(e : any) {
+    const file = (e.target as HTMLInputElement).files![0];
+
+    this.myForm.patchValue({
+      img: file
+    });
+
+    this.myForm.get('img')!.updateValueAndValidity()
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.fondoimagen = reader.result as string;
+    }
+    reader.readAsDataURL(file)
+  }
+
+
+  generarIMG(): void {
+    const DATA = document.getElementById('carta')
+    const options = {
+      allowTaint: false
+    };
+    html2canvas(DATA!,options).then(canvas => {
+      this.imagencreada = canvas.toDataURL(); 
+      this.downloadImage(this.imagencreada)
+    })
+  }
+downloadImage(data : string, filename = 'sintitulo.png'){
+let a = document.createElement('a');
+a.href = data;
+a.download = filename;
+document.body.appendChild(a);
+a.click()
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { NgxMatFileInputModule } from '@angular-material-components/file-input';
+import {MatButtonModule} from '@angular/material/button'
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  exports : [MaterialComponents],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialComponents,
+    NgxMatFileInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+      CUSTOM_ELEMENTS_SCHEMA,
+      NO_ERRORS_SCHEMA
+    ]
 })
 export class AppModule { }
